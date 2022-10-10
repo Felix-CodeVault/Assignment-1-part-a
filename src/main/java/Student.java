c8a4a194eaf50c825deccf208f8899e49ae26b5d
Felix-CodeVault <81178031+Felix-CodeVault@users.noreply.github.com>
2022-10-10 15:08:34 +0100

    First Commit and Push. Part a complete

---
+++ src/main/java/Student.java
@@ -0,0 +1,66 @@
+import java.time.LocalDate;
+
+public class Student {
+
+
+    private String fName, lName, username = getUsername();
+    private int age, ID;
+    private LocalDate DOB;
+
+    public String getUsername(){
+        return fName + lName + age;
+    }
+
+    public void setDetails(String fName, String lName, int age){
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+
+
+
+}
